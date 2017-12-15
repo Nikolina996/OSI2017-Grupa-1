ae8f806c86c14a3aa4ca3fd1d4605f4d56404c50
Zoran Pantoš <34402556+ZoranPantos@users.noreply.github.com>
2017-12-15 22:20:29 +0100

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -18,5 +18,49 @@ void User::userMenu() //basic menu
 
 void User::signUp()
 {
+  string name, surname, username;
+	string tmpName, tmpSurname, tmpUser;
+	char PIN[5], tmpPIN[5];
+	int check_user = 1, check_pin = 1, sum = 0;
+	cout << "Name: "; cin >> name;
+	cout << "Surname: "; cin >> surname;
+	ifstream file("korisnici.txt");
+	if (file.is_open())
+	{
+		do
+		{
+			cout << "Username: "; cin >> username;
+			while (!file.eof() && username != tmpUser)
+				file >> tmpName >> tmpSurname >> tmpUser >> tmpPIN;
+			if (tmpUser == username)
+				cout << "Username already exists. Please enter another." << endl;
+			else check_user = 0;
+		} while (check_user);
+	}
+	else
+	{
+		cout << "Could not open file 'korisnici.txt'." << endl;
+		return;
+	}
+	do
+	{
+		cout << "Password: "; cin >> PIN;
+		for (int i = 0; i < 4; i++)
+			if (isdigit(PIN[i])) sum++;
+		if (sum != 4)
+		{
+			cout << "Structure of password must be 4 positive numbers." << endl;
+			sum = 0;
+		}
+		else check_pin = 0;
+	} while (check_pin);
+	ofstream object("lista_cekanja.txt", ios::app);
+	if (object.is_open())
+	{
+		object << "\n" << name << ' ' << surname << ' ' << username << ' ' << PIN;
+		object.close();
+	}
+	else
+		cout << "Could not open file." << endl;
 }
 
