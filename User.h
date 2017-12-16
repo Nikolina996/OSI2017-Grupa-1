f2cdcb81083a7bc745c5c5c5f8c7caefaf58911b
Nikolina Maksimović <34402601+Nikolina996@users.noreply.github.com>
2017-12-16 19:33:37 +0100

    Update User.h

--- User.h
+++ User.h
@@ -5,13 +5,8 @@
 class User
 {
 public:
-	User(char *, char*, char*, char*);
-	~User();
 	virtual bool logIn() = 0;
 	virtual void userMenu();
 	virtual void setAccount() = 0;
 	virtual void signUp();
-protected:
-        char *name, *surname, *user_name, *password;
-
 };
