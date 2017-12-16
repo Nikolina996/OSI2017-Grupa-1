ef4e0ff124e97a88e742d205b74d470918db0ec9
Nikolina Maksimović <34402601+Nikolina996@users.noreply.github.com>
2017-12-16 19:03:36 +0100

    Update Administrator.cpp

--- Administrator.cpp
+++ Administrator.cpp
@@ -102,4 +102,54 @@ void Administrator::setUserAccount()
 
 void Administrator::addAccount()
 {
+	string name, surname, username, line; char PIN[5]; int input, numberOfRequests = 0; int c=NULL;
+	ifstream file("lista_cekanja.txt");
+	if (file.is_open())
+	{
+		c = file.peek(); 
+		if(c==-1)
+			cout << "No new registration requirements." << endl;
+		else
+		{
+			while (!file.eof())
+			{
+				getline(file, line); numberOfRequests++;
+			}
+			cout << "You have " << numberOfRequests << " new requests." << endl;
+			file.seekg(0);
+			ofstream object("korisnici.txt", ios::app); ofstream disapproved("lista_neodobrenih.txt", ios::app);
+			if (object.is_open() && disapproved.is_open())
+			{
+				while (!file.eof())
+				{
+					file >> name >> surname >> username >> PIN;
+					cout << endl << "Request from: " << endl;
+					cout << name << " " << surname << " " << username << " " << PIN << endl << endl;
+					do {
+						cout << "[1] Approve the registration request" << endl;
+						cout << "[0] Disapprove the registration request" << endl;
+						cin >> input;
+						if (input == 1) {
+							object << "\n" << name << ' ' << surname << ' ' << username << ' ' << PIN;
+							cout << "User has been successfully added to the user list" << endl;
+						}
+						else if (input == 0)
+						{
+							disapproved << "\n" << name << ' ' << surname << ' ' << username << ' ' << PIN;
+							cout << "The user has been added to the list of unauthorized users." << endl;
+						}
+					} while (input != 0 && input != 1);
+				}
+				object.close(); disapproved.close();
+			}
+			else
+				cout << "Could not open info file." << endl;
+		}
+	}
+	else
+		cout << "Could not open info file." << endl;
+	file.close();
+	std::ofstream ofs;
+	ofs.open("lista_cekanja.txt", std::ofstream::out | std::ofstream::trunc);
+	ofs.close();
 }
