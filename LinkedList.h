1de9eb3f19edab05e33b6372e6aef0a854e17d2d
Zoran Pantoš <34402556+ZoranPantos@users.noreply.github.com>
2018-01-15 18:57:18 +0100

    Create LinkedList.h

---
+++ LinkedList.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class LinkedList
+{
+private:
+	struct Article
+	{
+		string name, code;
+		double price, total;
+		int amount;
+		Article *next;
+		Article(string, string, int, double, double, Article*);
+		Article(string, string, int, double, double);
+	};
+	Article *head;
+	int numberOfNodes;
+	double inTotal, PDV, payment;
+	string billName, format;
+	void copy(const LinkedList&);
+	void move(LinkedList&&);
+public:
+	LinkedList();
+	LinkedList(const LinkedList&);
+	LinkedList(LinkedList&&);
+	~LinkedList();
+	LinkedList& operator = (const LinkedList&);
+	LinkedList& operator = (LinkedList&&);
+	void addArticle(string, string, int, double, double);
+	void inspect();
+	void inputErrorDescription(string);
+	void setBillData(double, double, double, string, string);
+	void printBillData();
+};
