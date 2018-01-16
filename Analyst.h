778f24849faf668eb516201629ea84bae8d0d28a
Nikolina Maksimović <34402601+Nikolina996@users.noreply.github.com>
2018-01-16 21:18:16 +0100

    Update Analyst.h
    Dodani prototipovi funkcija za obradu 4 formata računa i izmjenjeni prototipovi pomoćnih funkcija

--- Analyst.h
+++ Analyst.h
@@ -2,7 +2,8 @@
 using namespace std;
 #include<iostream>
 #include "User.h"
-class Analyst:public User
+#include "LinkedList.h"
+class Analyst:public User, public LinkedList
 {
 public:
 	void userMenu() override;
@@ -13,9 +14,11 @@ public:
 	void viewingDataForAParticularUser();
 	void reviewDataForASpecificProduct();
 	void viewDataForASpecificMonth();
-	void getBills();
-	string name();
+	void name();
 	string getname(string, string);
 	bool doesItExist(string, unsigned int, string);
+	void processingFormat1(string);
+	void processingFormat2(string);
+	void processingFormat3(string);
+	void processingFormat4(string);
 };
-
