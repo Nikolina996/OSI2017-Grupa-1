c79ecce9307535811c8d4d95f043558b7b1ab3f2
Nikolina Maksimović <34402601+Nikolina996@users.noreply.github.com>
2018-01-17 20:26:24 +0100

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,34 +1,29 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<conio.h>
 #include "User.h"
+#include"Windows.h"
 using namespace std;
 
 void User::userMenu() //basic menu
 {
-	int i; 
-	cout<<"/tMastermind Analyze"<<endl;
-	cout << "Enter the number of the option:" << endl << "[1] Log in" << endl << "[2] Sign up" << endl << "[0]Exit" << endl;
-	cin>>i;
-	if (i==0)
-	   return; 
-	else if (i==1)
-	    logIn();
-	else if (i==2)
-	     singUp();
-	else
-	     userMenu();
+	system("CLS");
+	cout << "\t\tMastermind Analyze" << endl << endl;
+	cout << "Enter the number of the option:" << endl << "[1] Log in" << endl << "[2] Sign up" << endl << "[0] Exit" << endl;
 }
 
-void User::signUp()
+void User::signUp() //Function for registration
 {
-  string name, surname, username;
+	system("CLS");
+	cout << "\t\tMastermind Analyze" << endl << endl;
+	string name, surname, username, PIN, tmpPIN;
 	string tmpName, tmpSurname, tmpUser;
-	char PIN[5], tmpPIN[5];
 	int check_user = 1, check_pin = 1, sum = 0;
+	char ch;
 	cout << "Name: "; cin >> name;
 	cout << "Surname: "; cin >> surname;
-	ifstream file("korisnici.txt");
+	ifstream file("C:\\MasterMindAnalyze\\Users\\Users.txt");
 	if (file.is_open())
 	{
 		do
@@ -36,21 +31,30 @@ void User::signUp()
 			cout << "Username: "; cin >> username;
 			while (!file.eof() && username != tmpUser)
 				file >> tmpName >> tmpSurname >> tmpUser >> tmpPIN;
-			if (tmpUser == username)
+			if (tmpUser == username) { //Checking if the same username already exists
 				cout << "Username already exists. Please enter another." << endl;
+				Sleep(2000);
+			}
 			else check_user = 0;
 		} while (check_user);
 	}
 	else
 	{
-		cout << "Could not open file 'korisnici.txt'." << endl;
+		cout << "Could not open info file" << endl;
+		Sleep(2000);
 		return;
 	}
 	do
 	{
-		cout << "Password: "; cin >> PIN;
+		cout << "Password: "; 
+		ch = _getch();
+		while (ch != 13) {//character 13 is enter
+			PIN.push_back(ch);
+			cout << '*';
+			ch = _getch();
+		}
 		for (int i = 0; i < 4; i++)
-			if (isdigit(PIN[i])) sum++;
+			if (isdigit(PIN[i])) sum++; //Checks if the entered character is a number
 		if (sum != 4)
 		{
 			cout << "Structure of password must be 4 positive numbers." << endl;
@@ -58,13 +62,26 @@ void User::signUp()
 		}
 		else check_pin = 0;
 	} while (check_pin);
-	ofstream object("lista_cekanja.txt", ios::app);
+	PIN.shrink_to_fit();
+	ofstream object("C:\\MasterMindAnalyze\\Users\\Waiting_list.txt", ios::app);
 	if (object.is_open())
 	{
-		object << "\n" << name << ' ' << surname << ' ' << username << ' ' << PIN;
+		object << name << ' ' << surname << ' ' << username << ' ' << PIN << "\n";
 		object.close();
+		cout << endl << "Your registration request has been successfully sent." << endl;
+		Sleep(2000);
 	}
 	else
+	{
 		cout << "Could not open file." << endl;
+		Sleep(2000);
+	}
 }
 
+void User::logInMenu() //submenu
+{
+	system("CLS");
+	cout << "\t\tMastermind Analyze" << endl << endl;
+	cout << "\tLog in menu" << endl;
+	cout << "Enter the number of the option:" << endl << "[1] Administrator" << endl << "[2] Analyst" << endl << "[0] Back" << endl;
+}
